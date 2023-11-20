38e52a9605677c720830da45eb0d121830dda060
Durvesh27 <132134129+Durvesh27@users.noreply.github.com>
2023-11-20 23:34:50 +0530

    Updated

--- quiz-app/src/index.js
+++ quiz-app/src/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import AuthProvider from './MyContext';
-import { ChakraProvider } from '@chakra-ui/react'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./MyContext";
+import { ChakraProvider } from "@chakra-ui/react";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<>
-<BrowserRouter>
-<AuthProvider>
-<ChakraProvider>
-  <App/>
-  </ChakraProvider>
-</AuthProvider>
-</BrowserRouter>
- </>
-)
-
+  <>
+    <BrowserRouter>
+      <AuthProvider>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </>
+);
