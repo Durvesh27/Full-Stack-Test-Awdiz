172b4d5d297da5569acae8424bc0aba5315c48b1
Durvesh27 <132134129+Durvesh27@users.noreply.github.com>
2023-12-02 13:43:44 +0530

    Updated

--- quiz-app/src/MyContext.js
+++ quiz-app/src/MyContext.js
@@ -1,75 +1,59 @@
-import axios from "axios";
+
 import { useState } from "react";
 import { useEffect } from "react";
 import { useReducer } from "react";
 import { createContext } from "react";
 import api from "./Components/Api Config";
 
-export const AuthContext=createContext()
+export const AuthContext = createContext();
 
-const initialvalue={user:null}
+const initialvalue = { user: null };
 
-// function reducer(state,action){
-// if(action.type==="login"){
-// return {...state,user:action.payload}
-// }else if(action.type==="logout"){
-// return {...state,user:null}
-// }
-// else{
-// return state
-// }
-// }
-
-function reducer(state,action){
-  switch(action.type){
-  case "login":
-  return {...state,user:action.payload}
-  case "logout":
-  return {...state,user:null}
-  default:
-  return state
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload };
+    case "logout":
+      return { ...state, user: null };
+    default:
+      return state;
   }
+}
+
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialvalue);
+  function Login(data) {
+    if (data) {
+      dispatch({
+        type: "login",
+        payload: data,
+      });
+    }
   }
 
-const AuthProvider=({children})=>{
-const[state,dispatch]=useReducer(reducer,initialvalue)
-const[questions,setQuestions]=useState([])
-function Login(data){
-if(data){
-dispatch({
-type:"login",
-payload:data
-})
-}
-}
+  function Logout() {
+    localStorage.removeItem("QuizToken");
+    dispatch({
+      type: "logout",
+    });
+  }
 
-function Logout (){
-localStorage.removeItem("QuizToken")
-dispatch({
-type:"logout"
-})
-}
-
-useEffect(() => {
+  useEffect(() => {
     const getCurrentUserData = async () => {
       const token = JSON.parse(localStorage.getItem("QuizToken"));
-      if(token){
-        try{
-          const response = await api.post(
-            "/current-user",
-            { token }
-          );
+      if (token) {
+        try {
+          const response = await api.post("/current-user", { token });
           if (response.data.success) {
             dispatch({
               type: "login",
-              payload: response.data.user
+              payload: response.data.user,
             });
-          } 
+          }
+        } catch (error) {
+          console.log(error);
         }
-        catch(error){
-          console.log(error)
-        }
-      }else{
+      } else {
         dispatch({
           type: "logout",
         });
@@ -78,13 +62,11 @@ useEffect(() => {
     getCurrentUserData();
   }, []);
 
-return(
-<AuthContext.Provider value={{Login,Logout,state,questions,setQuestions}}>
-    {children}
-</AuthContext.Provider>
-)
+  return (
+    <AuthContext.Provider value={{ Login, Logout, state }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-}
-
-export default AuthProvider
-
+export default AuthProvider;
